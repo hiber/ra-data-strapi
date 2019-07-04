742859247b38094fcb8e5e921a1289b3b2cb959d
hibersh <hibersh@gmail.com>
2019-07-04 11:15:15 +0800

    CREATE json reponse than request body

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { stringify } from 'query-string';
 import {
-    fetchUtils,
-    GET_LIST,
-    GET_ONE,
-    GET_MANY,
-    GET_MANY_REFERENCE,
-    CREATE,
-    UPDATE,
-    UPDATE_MANY,
-    DELETE,
-    DELETE_MANY,
+  fetchUtils,
+  GET_LIST,
+  GET_ONE,
+  GET_MANY,
+  GET_MANY_REFERENCE,
+  CREATE,
+  UPDATE,
+  UPDATE_MANY,
+  DELETE,
+  DELETE_MANY,
 } from 'react-admin';
 
 /**
@@ -25,141 +25,141 @@ import {
  * DELETE       => DELETE http://my.api.url/posts/123
  */
 export default (apiUrl, httpClient = fetchUtils.fetchJson) => {
-    /**
-     * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
-     * @param {String} resource Name of the resource to fetch, e.g. 'posts'
-     * @param {Object} params The data request params, depending on the type
-     * @returns {Object} { url, options } The HTTP request parameters
-     */
-    const convertDataRequestToHTTP = (type, resource, params) => {
-        let url = '';
-        const options = {};
-        switch (type) {
-            case GET_LIST: {
-                const { page, perPage } = params.pagination;
-                const { field, order } = params.sort;
-                const { q, ...filter } = params.filter;
-                const query = {
-                    ...fetchUtils.flattenObject(filter),
-                    _sort: field + ':' + order,
-                    _start: (page - 1) * perPage,
-                    _limit: perPage,
-                };
-                if (q) query['_q'] = q;
-                url = `${apiUrl}/${resource}?${stringify(query)}`;
-                break;
-            }
-            case GET_ONE:
-                url = `${apiUrl}/${resource}/${params.id}`;
-                break;
-            case GET_MANY_REFERENCE: {
-                const { page, perPage } = params.pagination;
-                const { field, order } = params.sort;
-                const query = {
-                    ...fetchUtils.flattenObject(params.filter),
-                    [params.target]: params.id,
-                    _sort: field + ':' + order,
-                    _start: (page - 1) * perPage,
-                    _limit: perPage,
-                };
-                url = `${apiUrl}/${resource}?${stringify(query)}`;
-                break;
-            }
-            case UPDATE:
-                url = `${apiUrl}/${resource}/${params.id}`;
-                options.method = 'PUT';
-                options.body = JSON.stringify(params.data);
-                break;
-            case CREATE:
-                url = `${apiUrl}/${resource}`;
-                options.method = 'POST';
-                options.body = JSON.stringify(params.data);
-                break;
-            case DELETE:
-                url = `${apiUrl}/${resource}/${params.id}`;
-                options.method = 'DELETE';
-                break;
-            case GET_MANY: {
-                const query = {
-                    _id_in: params.ids.map(id => typeof id === 'string' ? id : id.id),
-                };
-                url = `${apiUrl}/${resource}?${stringify(query)}`;
-                break;
-            }
-            default:
-                throw new Error(`Unsupported fetch action type ${type}`);
-        }
-        return { url, options };
-    };
+  /**
+   * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
+   * @param {String} resource Name of the resource to fetch, e.g. 'posts'
+   * @param {Object} params The data request params, depending on the type
+   * @returns {Object} { url, options } The HTTP request parameters
+   */
+  const convertDataRequestToHTTP = (type, resource, params) => {
+    let url = '';
+    const options = {};
+    switch (type) {
+      case GET_LIST: {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const { q, ...filter } = params.filter;
+        const query = {
+          ...fetchUtils.flattenObject(filter),
+          _sort: field + ':' + order,
+          _start: (page - 1) * perPage,
+          _limit: perPage,
+        };
+        if (q) query['_q'] = q;
+        url = `${apiUrl}/${resource}?${stringify(query)}`;
+        break;
+      }
+      case GET_ONE:
+        url = `${apiUrl}/${resource}/${params.id}`;
+        break;
+      case GET_MANY_REFERENCE: {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+          ...fetchUtils.flattenObject(params.filter),
+          [params.target]: params.id,
+          _sort: field + ':' + order,
+          _start: (page - 1) * perPage,
+          _limit: perPage,
+        };
+        url = `${apiUrl}/${resource}?${stringify(query)}`;
+        break;
+      }
+      case UPDATE:
+        url = `${apiUrl}/${resource}/${params.id}`;
+        options.method = 'PUT';
+        options.body = JSON.stringify(params.data);
+        break;
+      case CREATE:
+        url = `${apiUrl}/${resource}`;
+        options.method = 'POST';
+        options.body = JSON.stringify(params.data);
+        break;
+      case DELETE:
+        url = `${apiUrl}/${resource}/${params.id}`;
+        options.method = 'DELETE';
+        break;
+      case GET_MANY: {
+        const query = {
+          _id_in: params.ids.map(id => typeof id === 'string' ? id : id.id),
+        };
+        url = `${apiUrl}/${resource}?${stringify(query)}`;
+        break;
+      }
+      default:
+        throw new Error(`Unsupported fetch action type ${type}`);
+    }
+    return { url, options };
+  };
 
-    /**
-     * @param {Object} response HTTP response from fetch()
-     * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
-     * @param {String} resource Name of the resource to fetch, e.g. 'posts'
-     * @param {Object} params The data request params, depending on the type
-     * @returns {Object} Data response
-     */
-    const convertHTTPResponse = (response, type, resource, params) => {
-        const { headers, json } = response;
-        switch (type) {
-            case GET_LIST:
-            case GET_MANY_REFERENCE:
-                const { q, ...filter } = params.filter;
-                if (q) filter['_q'] = q;
-                return httpClient(`${apiUrl}/${resource}/count?${stringify(fetchUtils.flattenObject(filter))}`, {
-                    method: 'GET'
-                 }).then(response => {
-                    return {
-                        data: json,
-                        total: response.json
-                    };
-                 });
-            case CREATE:
-                return { data: { ...params.data, id: json.id } };
-            default:
-                return { data: json };
-        }
-    };
+  /**
+   * @param {Object} response HTTP response from fetch()
+   * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
+   * @param {String} resource Name of the resource to fetch, e.g. 'posts'
+   * @param {Object} params The data request params, depending on the type
+   * @returns {Object} Data response
+   */
+  const convertHTTPResponse = (response, type, resource, params) => {
+    const { headers, json } = response;
+    switch (type) {
+      case GET_LIST:
+      case GET_MANY_REFERENCE:
+        const { q, ...filter } = params.filter;
+        if (q) filter['_q'] = q;
+        return httpClient(`${apiUrl}/${resource}/count?${stringify(fetchUtils.flattenObject(filter))}`, {
+          method: 'GET'
+        }).then(response => {
+          return {
+            data: json,
+            total: response.json
+          };
+        });
+      case CREATE:
+        return { data: { ...json, id: json.id } };
+      default:
+        return { data: json };
+    }
+  };
 
-    /**
-     * @param {string} type Request type, e.g GET_LIST
-     * @param {string} resource Resource name, e.g. "posts"
-     * @param {Object} payload Request parameters. Depends on the request type
-     * @returns {Promise} the Promise for a data response
-     */
-    return (type, resource, params) => {
-        // json-server doesn't handle filters on UPDATE route, so we fallback to calling UPDATE n times instead
-        if (type === UPDATE_MANY) {
-            return Promise.all(
-                params.ids.map(id =>
-                    httpClient(`${apiUrl}/${resource}/${id}`, {
-                        method: 'PUT',
-                        body: JSON.stringify(params.data),
-                    })
-                )
-            ).then(responses => ({
-                data: responses.map(response => response.json),
-            }));
-        }
-        // json-server doesn't handle filters on DELETE route, so we fallback to calling DELETE n times instead
-        if (type === DELETE_MANY) {
-            return Promise.all(
-                params.ids.map(id =>
-                    httpClient(`${apiUrl}/${resource}/${id}`, {
-                        method: 'DELETE',
-                    })
-                )
-            ).then(responses => ({
-                data: responses.map(response => response.json),
-            }));
-        }
-        const { url, options } = convertDataRequestToHTTP(
-            type,
-            resource,
-            params
-        );
-        return httpClient(url, options).then(response =>
-            convertHTTPResponse(response, type, resource, params)
-        );
-    };
+  /**
+   * @param {string} type Request type, e.g GET_LIST
+   * @param {string} resource Resource name, e.g. "posts"
+   * @param {Object} payload Request parameters. Depends on the request type
+   * @returns {Promise} the Promise for a data response
+   */
+  return (type, resource, params) => {
+    // json-server doesn't handle filters on UPDATE route, so we fallback to calling UPDATE n times instead
+    if (type === UPDATE_MANY) {
+      return Promise.all(
+        params.ids.map(id =>
+          httpClient(`${apiUrl}/${resource}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+          })
+        )
+      ).then(responses => ({
+        data: responses.map(response => response.json),
+      }));
+    }
+    // json-server doesn't handle filters on DELETE route, so we fallback to calling DELETE n times instead
+    if (type === DELETE_MANY) {
+      return Promise.all(
+        params.ids.map(id =>
+          httpClient(`${apiUrl}/${resource}/${id}`, {
+            method: 'DELETE',
+          })
+        )
+      ).then(responses => ({
+        data: responses.map(response => response.json),
+      }));
+    }
+    const { url, options } = convertDataRequestToHTTP(
+      type,
+      resource,
+      params
+    );
+    return httpClient(url, options).then(response =>
+      convertHTTPResponse(response, type, resource, params)
+    );
+  };
 };
